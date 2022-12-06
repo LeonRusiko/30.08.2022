ce079705376f3552a33e6fad02b4a9909d1b0466
Aituar Semeibek <113921403+LeonRusiko@users.noreply.github.com>
2022-12-07 01:27:28 +0600

    Update task 10.js

--- task 10.js
+++ task 10.js
@@ -1,1 +1,5 @@
-# Homework-2
+let date = prompt('Enter the date - day-month-year ');
+date = date.split('-');
+let oldDate = new Date(date[2], date[1]-1, date[0]);
+oldDate.setDate(oldDate.getDate()+1);
+alert(oldDate);
