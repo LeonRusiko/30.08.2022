f98e32ce170ee1818973e1af320f06468e08579e
Aituar Semeibek <113921403+LeonRusiko@users.noreply.github.com>
2022-12-07 01:08:44 +0600

    Update and rename README.md to task 1.js

---
+++ task 1.js
@@ -0,0 +1,64 @@
+let age = prompt();
+switch (age) {
+    case '0':
+    case '1':
+    case '2':
+        alert('child');
+        break;
+    case '12':   
+    case '13':
+    case '14':
+    case '15':   
+    case '16':
+    case '17':
+    alert('teenager')
+        break;
+    case '18':
+    case '19':
+    case '20':
+    case '21':   
+    case '22':
+    case '23':
+    case '24':   
+    case '25':
+    case '26':   
+    case '27':
+    case '28':
+    case '29':   
+    case '30':
+    case '31':
+    case '32':   
+    case '33':
+    case '34':
+    case '35':
+    case '36':
+    case '37':   
+    case '38':
+    case '39':
+    case '40':
+    case '41':
+    case '42':   
+    case '43':
+    case '44':   
+    case '45':
+    case '46':
+    case '47':   
+    case '48':
+    case '49':
+    case '50':   
+    case '51':
+    case '52':
+    case '53':
+    case '54':
+    case '55':   
+    case '56':
+    case '57':
+    case '58':   
+    case '59':
+    case '60':
+        alert('adult');
+        break;
+    default:
+        alert('old');
+        break;
+}
